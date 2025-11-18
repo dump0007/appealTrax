de9182a30e1e86dfe9b63c5b020349c06fc490ff
dump0007 <dump.bin0007@gmail.com>
2025-11-18 16:07:04 +0530

    pm2

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
     hmr: {
+      host: '0.0.0.0',
+      port: 5173,
+      protocol: 'ws',
       overlay: false,
     },
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    strictPort: true,
   },
 })
